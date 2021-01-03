63c35bfb0cdc95df593f29845470df50dbb5a913
benitodarder@gmail.com <benitodarder@gmail.com>
2021-01-03 21:39:24 +0100

    Improves README.md... Again

--- customPackages/packages/setup.py
+++ customPackages/packages/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/benitodarder/python-workshop/customPackages",
+    url="https://github.com/benitodarder/python-workshop",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
